1a3ffd74eb30e271aec24ca733cb91cd336dc697
jpol <jpol@yondu.com>
2019-05-02 14:14:44 +0800

    create a new response that handdle missing token ,and  blacklisted token

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -49,21 +49,38 @@ class Handler extends ExceptionHandler
     public function render($request, Exception $exception)
     {
         if ($exception instanceof \Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException) {
+            if (is_null($exception->getPrevious())){
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'Token is missing'
+                ], $exception->getStatusCode());
+            }
             switch (get_class($exception->getPrevious())) {
                 case \Tymon\JWTAuth\Exceptions\TokenExpiredException::class:
                     return response()->json([
                         'status' => 'error',
-                        'message' => 'Token has expired',
+                        'message' => 'Token has been expired',
                         'data' => []
                     ], $exception->getStatusCode());
                 case \Tymon\JWTAuth\Exceptions\TokenInvalidException::class:
-                case \Tymon\JWTAuth\Exceptions\TokenBlacklistedException::class:
                     return response()->json([
                         'status' => 'error',
                         'message' => 'Token is invalid'
                     ], $exception->getStatusCode());
+                case \Tymon\JWTAuth\Exceptions\TokenBlacklistedException::class:
+                    return response()->json([
+                        'status' => 'error',
+                        'message' => 'Token is on Blacklist'
+                    ], $exception->getStatusCode());
                 default:
                     break;
+            }
+        } elseif ($exception instanceof \Spatie\Permission\Exceptions\UnauthorizedException) {
+            if (is_null($exception->getPrevious())){                
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'You don\'t have a permission to access this API' 
+                ], $exception->getStatusCode());
             }
         }
         // if ($exception instanceof TokenExpiredException ) {
